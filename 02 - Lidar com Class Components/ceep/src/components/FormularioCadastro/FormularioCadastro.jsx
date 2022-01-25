c73d3cd832e6adba36bea72a351ea4b79ca1d627
Lucas Souza <lucassouzzamarques@gmail.com>
2022-01-25 17:19:30 -0300

    Seguindo padrões

--- 02 - Lidar com Class Components/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ 02 - Lidar com Class Components/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -5,11 +5,17 @@ class FormularioCadastro extends Component {
     super(props);
     this.titulo = "";
     this.texto = "";
+    this.categoria = "Sem Categoria";
   }
 
   _handleMudancaTitulo(evento) {
     evento.stopPropagation();
     this.titulo = evento.target.value;
+  }
+
+  _handleMudancaCategoria(evento) {
+    evento.stopPropagation();
+    this.categoria = evento.target.value;
   }
 
   _handleMudancaTexto(evento) {
@@ -20,12 +26,24 @@ class FormularioCadastro extends Component {
   _criarNota(evento) {
     evento.preventDefault();
     evento.stopPropagation();
-    this.props.criarNota(this.titulo, this.texto);
+    this.props.criarNota(this.titulo, this.texto, this.categoria);
   }
 
   render() {
     return (
       <form className="form-cadastro" onSubmit={this._criarNota.bind(this)}>
+        
+        <select 
+          onChange={this._handleMudancaCategoria.bind(this)} 
+          className="form-cadastro_input">
+          
+          <option> Sem Categoria </option>
+
+          {this.props.categorias.map(categoria => {
+            return <option> {categoria} </option>
+          })}
+        </select>
+        
         <input
           type="text"
           placeholder="Título"
